4215c4401ae034bb6d8b6ebbdd775b643577f11b
CodyBrettWatson <cody.brett.watson@gmail.com>
2019-03-08 11:16:47 -0600

    added v1.0 of checkbook_main

---
+++ checkbook_main.py
@@ -0,0 +1,23 @@
+def welcome_statement():
+    print('Welcome Customer!')
+
+def action_statement():
+    print('''
+    Would you like to:
+    1) View current balance
+    2) Make a withdrawal (debit)
+    3) Make a deposit (credit)
+    4) Exit
+    ''')
+    # make any other choice invalid and then promt the user
+    # for another action.
+
+
+def current_balance():
+    #displays the current balance of the account
+def make_withdrawal():
+    #asks the user how much they would like to withdrawal
+def make_deposit():
+    #asks the user how much they would like to deposit
+def exit():
+    #exits out of the application
